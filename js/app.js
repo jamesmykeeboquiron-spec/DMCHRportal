7263e2a8c075072a7d61fb6eb1f044b4350d2d35
jamesmykeeboquiron-spec <jamesmykeeboquiron@gmail.com>
2026-05-22 13:10:32 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+function updateDateTime() {
+  const clockElement = document.getElementById("clock");
+  const dateElement = document.getElementById("date");
 
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  });
+
+  const date = now.toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+  });
+
+  clockElement.textContent = time;
+  dateElement.textContent = date;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
